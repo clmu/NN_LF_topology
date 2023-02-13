3c90baea8ad3abeb99d9d02a8c058d5da2841a56
clmu <clmensmuller@tuta.io>
2023-02-13 12:04:51 +0100

    Function to evaluate the percentage error of each model output

---
+++ Neural_network/data_evaluation.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def eval_model_performance(nr_results, model_results, path=None):
+
+    '''
+    function calculates the model performance by calculating the percentage error of each output.
+    note: this function could likely be implemented as error function in the model itself.
+    :param nr_results: filename of the results from the nr verification file.
+    :param model_results: filename of the file containing the model prediction data.
+    :param results_path: path containg the two files above.
+    :return: array containing the percentage errors of all model outputs.
+    '''
+
+    if path is not None:
+        nr_results = path + nr_results
+        model_results = path + model_results
+
+    nr_results = np.load(nr_results)
+    model_results = np.load(model_results)
+    samples, outputs = np.shape(nr_results)
+
+    percentage_error_matrix = np.zeros((samples, outputs), dtype=float)
+    for i in range(samples):
+        percentage_error_matrix[i] = (model_results[i] - nr_results[i]) / nr_results[i] * 100
+
+    return np.average(percentage_error_matrix, axis=0)
+
+avg_nr_runtime    = 0.00165041
+avg_model_runtime = 0.00166583
+
+results_path = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/NN_results/'
+
+output_performance = eval_model_performance('verification_outputs.npy', 'model_predictions.npy', path=results_path)
+avg_output_performance = np.average(output_performance)
