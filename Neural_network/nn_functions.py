4bdecca8d343b6c6bad6796cf9aeea9f7de336e1
clmu <clmensmuller@tuta.io>
2023-02-14 17:00:03 +0100

    Initial documentation

--- Neural_network/nn_functions.py
+++ Neural_network/nn_functions.py
@@ -4,6 +4,12 @@ import tensorflow as tf
 
 def verification_predictions(verification_input_data, model):
 
+    '''
+    function to calculate predictions for all verification input data.
+    :param verification_input_data: the input data for calculating predictions.
+    :param model: Model to be used for calculating the predictions.
+    :return: matrix  ??  of all predictions and average prediction time.
+    '''
     v_samples, v_variables = np.shape(verification_input_data)
     model_prediction_time = np.zeros((v_samples,))
     model_predictions = np.zeros((v_samples, v_variables))
@@ -19,6 +25,18 @@ def verification_predictions(verification_input_data, model):
     return model_predictions, avg_model_prediction_time
 
 def import_data(inputfilename, outputfilename, datapath='', verification_fraction=0.2, norm_input=1, norm_output=1):
+
+    '''
+    Function to import data for generating neural networks.
+    :param inputfilename:
+    :param outputfilename:
+    :param datapath:
+    :param verification_fraction:
+    :param norm_input:
+    :param norm_output:
+    :return:
+    '''
+
     inputdata = np.load(datapath + inputfilename)
     outputdata = np.load(datapath + outputfilename)
 
@@ -30,6 +48,10 @@ def import_data(inputfilename, outputfilename, datapath='', verification_fractio
     return (l_in, l_out), (v_in, v_out)
 
 def gen_model():
+    '''
+    Function to generate a neural network. Useful to make sure that all networks have the same architecture.
+    :return:
+    '''
     nn = tf.keras.models.Sequential([
         tf.keras.layers.Flatten(input_shape=(6,)),
         # tf.keras.layers.Dense(6, activation='relu'),
