be8abaa8b2223581f38f2e2b3f1c925594abf79e
clmu <clmensmuller@tuta.io>
2023-02-14 16:57:02 +0100

    Changing function so it accepts both filenames and variables

--- Neural_network/data_evaluation.py
+++ Neural_network/data_evaluation.py
@@ -5,18 +5,19 @@ def eval_model_performance(nr_results, model_results, path=None):
     '''
     function calculates the model performance by calculating the percentage error of each output.
     note: this function could likely be implemented as error function in the model itself.
-    :param nr_results: filename of the results from the nr verification file.
-    :param model_results: filename of the file containing the model prediction data.
+    :param nr_results: filename or variable of the results from the nr verification file.
+    :param model_results: filename or variable of the file containing the model prediction data.
     :param results_path: path containg the two files above.
     :return: array containing the percentage errors of all model outputs.
     '''
 
-    if path is not None:
-        nr_results = path + nr_results
-        model_results = path + model_results
+    if isinstance(nr_results, str) and isinstance(model_results, str):
+        if path is not None:
+            nr_results = path + nr_results
+            model_results = path + model_results
+            nr_results = np.load(nr_results)
+            model_results = np.load(model_results)
 
-    nr_results = np.load(nr_results)
-    model_results = np.load(model_results)
     samples, outputs = np.shape(nr_results)
 
     percentage_error_matrix = np.zeros((samples, outputs), dtype=float)
@@ -31,4 +32,5 @@ avg_model_runtime = 0.00166583
 results_path = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/NN_results/'
 
 output_performance = eval_model_performance('verification_outputs.npy', 'model_predictions.npy', path=results_path)
-avg_output_performance = np.average(output_performance)
+abs_output_performance = np.abs(output_performance)
+avg_output_performance = np.average(abs_output_performance)
