7155126bcdd20accc56da766ef6eec9831cc65b3
clmu <clmensmuller@tuta.io>
2023-02-15 15:49:00 +0100

    Used to test percentage error data from evaluate checkpoints.

--- Tests/test.py
+++ Tests/test.py
@@ -1,9 +1,15 @@
 import numpy as np
-from Neural_network.nn_functions import import_data, verification_predictions, gen_model
-from Neural_network.data_evaluation import eval_model_performance
+from Neural_network.data_evaluation import evaluate_checkpoints # denne laster tensorflow på et vis
+from Neural_network.nn_functions import import_data
+import matplotlib.pyplot as plt
+
+percentage_error_matrix = np.load('/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/percentage_accuracy_matrix.npy')
+
+avgs, maxs= np.average(percentage_error_matrix, axis=0), np.amax(percentage_error_matrix, axis=0)
+
+array_of_predictions = np.load('/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/array_of_predictions.npy')
 
 datapath = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/'
-cp_path = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/checkpoints/'
 input = np.load(datapath + 'simple data.npy')
 output = np.load(datapath + 'simple o data.npy')
 
@@ -13,13 +19,36 @@ norm_outputs = 10
 
 (l_i, l_o), (v_i, v_o) = import_data('simple data.npy', 'simple o data.npy', datapath=datapath, norm_input=norm_inputs, norm_output=norm_outputs)
 
-cp15 = gen_model()
-cp9 = gen_model()
-cp15.load_weights(cp_path + 'cp_0150.ckpt')
-cp9.load_weights(cp_path + 'cp_0090.ckpt')
+accuracy_vectors = np.array(evaluate_checkpoints(v_o, array_of_predictions))
 
-predictions_15, times_15 = verification_predictions(v_i, cp15)
-predictions_9, times_9 = verification_predictions(v_i, cp9)
+np.save(datapath + 'accuracy_vectors.npy', accuracy_vectors)
+avg_accuracies = np.average(accuracy_vectors[0], axis=1)
+avg_worst = np.average(accuracy_vectors[1], axis=1)
 
-accuracy_cp15 = np.array(eval_model_performance(v_o, predictions_15))
-accuracy_cp9 = np.array(eval_model_performance(v_o, predictions_9))
+'''
+Plotting
+'''
+
+epochs = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
+
+#plt.subplots(figsize=(6,2))
+cm = 1/2.54
+fig, ax1 = plt.subplots(figsize=(16*cm, 10*cm), dpi=300)
+ax2 = ax1.twinx()
+ax1.set_xlabel('Epochs')
+ax1.set_ylabel('Average percentage deviation')
+ax2.set_ylabel('Worst percentage deviation')
+avg = ax1.plot(epochs, avg_accuracies, color='blue', label='avg')
+worst = ax2.plot(epochs, avg_worst, linestyle='dotted', color='red', label='worst')
+'''
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+'''
+lbs = avg+worst
+labels = [l.get_label() for l in lbs]
+ax1.legend(avg+worst, labels, loc='upper right')
+plt.savefig('epochs_accuracy', format='png')
+plt.show()
+
+
+print('finito')
