644ba7d7bfd1bcb3fa240243a0913573e51018af
clmu <clmensmuller@tuta.io>
2023-03-21 16:27:04 +0100

    Using file to analyze seeral checkpoints

--- Neural_network/obj_eval_checkpoints.py
+++ Neural_network/obj_eval_checkpoints.py
@@ -2,16 +2,17 @@ import tensorflow as tf
 import numpy as np
 
 from Neural_network.NN_objects import NeuralNetwork as NN
+from Neural_network.NN_objects import pickle_store_object as store
 def gen_filename_ext(number):
     number = str(number)
     while len(number) < 4:
         number = '0' + number
-    number = number + '.index'
+    number = 'cp_' + number# + '.index'
     return number
 
 
 path_to_data = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/'
-cp_path_custom_loss = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/cp_small_CustomLoss/cp_'
+cp_path_custom_loss = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/cp_small_SquaredLineFlowLoss/'
 architecture = [6, 12, 12, 12, 6]
 
 model_numbers = np.arange(1, 30+1)
@@ -33,11 +34,18 @@ for model in models:
     model.loss_fn = tf.keras.losses.MSE
     model.init_nn_model_dynamic(architecture=architecture, const_l_rate=True)
 
-
+thresholds = [20, 10, 5, 3]
+avg_perc_dev = np.zeros(len(models), dtype=float)
+performance_dicts = []
 for index in range(len(models)):
-    path = cp_path_custom_loss + file_exts[index]
     models[index].tf_model.load_weights(cp_path_custom_loss + file_exts[index])
+    models[index].model_pred()
+    for threshold in thresholds:
+        models[index].generate_performance_data_dict(threshold)
+    performance_dicts.append(models[index].performance_dict)
+    avg_perc_dev[index] = models[index].performance_dict['3percent']['average']
 
+store(performance_dicts, path=cp_path_custom_loss, filename='performance_dicts')
 
 
 
