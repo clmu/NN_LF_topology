9672048327249cc9eb15cb7149cbef92b6d6b20a
clmu <clmensmuller@tuta.io>
2023-03-31 15:21:05 +0200

    started work on improving userfriendliness.

--- Neural_network/NN_obj_based.py
+++ Neural_network/NN_obj_based.py
@@ -1,25 +1,22 @@
-'''
+"""
 
 This is the master script for training and evaluating a neural network from the object oriented approach.
 
 Custom loss functions are found in Neural_network.custom_loss_function. if others are to be used, refer to keras doc.
 
 
-'''
-
+"""
 import os
-import time
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #TO SILCENCE INITIAL WARNINGS.
 import tensorflow as tf
-import numpy as np
-import pickle
-import math
-
-from Neural_network.NN_objects import NeuralNetwork as NN
+from Neural_network.NN_objects import NeuralNetwork
 from Neural_network.custom_loss_function import loss_acc_for_lineflows,\
     CustomLoss, SquaredLineFlowLoss, LineFlowLossForAngle
 from Neural_network.NN_objects import pickle_store_object as store
 from Neural_network.NN_objects import load_architecture
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #TO SILCENCE INITIAL WARNINGS.
+
+
+
 
 def load_loss_function(loss_fun_name, path_to_sys_file=''):
     if loss_fun_name == 'MSE':
@@ -35,8 +32,6 @@ def load_loss_function(loss_fun_name, path_to_sys_file=''):
 
 def set_params_and_init_nn(model, data_in_name='', data_out_name='', pickle_load=False):
     # NN parameters
-    model.epochs = 60
-    model.batch_size = 10
     model.initializer = tf.keras.initializers.glorot_uniform(seed=0)
                                             # THIS IS THE SAME AS USED IN NON OBJ BASED APPROACH.
     model.init_data(data_in_name,
@@ -64,71 +59,115 @@ list_of_nn_objs = [nn_regular_mse, nn_custom_loss, nn_square_loss, nn_angle_loss
 
 architecture = [6, 12, 12, 12, 6]'''
 
-proj_folder = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) #cwd = current working dir, pardir = parent dir
 
-nn_obj = NN()
+
+proj_folder = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) #cwd = current working dir, pardir = parent dir
+path_sys_file = proj_folder + '/LF_3bus' + '/IEEE33BusDSAL.xls'
+path_data = proj_folder + '/Neural_network/datasets/'
+
+folder_hierarchy = {}
+
+nn_obj = NeuralNetwork()
 
 path_to_system_description_file = proj_folder + '/LF_3bus/'#'/home/clemens/PycharmProjects/NN_LF_Topology/LF_3bus/' #'/home/clemens/Dropbox/EMIL_MIENERG21/Master/IEEE33bus_69bus/IEEE33BusDSAL.xls'
-path_to_system_description_file += 'IEEE33BusDSAL.xls'
+sys_filename = 'IEEE33BusDSAL.xls'
 path_to_data = proj_folder + '/Neural_network/datasets/'#'/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/datasets/'
-#network_name = 'medium'
+path_to_cp_folder = proj_folder + '/Neural_network/checkpoints'
+dataset = '' #type slim if slim dataset is desired.
 network_name = 'medium'
-#network_name = 'medium_slim_learn0.5e-3'
-#network_name = 'large'
-network_loss_function = 'MSE' #CustomLoss, SquaredLineFlowLoss, LineFlowLossForAngle
-input_data_name = network_name + '_inputs.obj'
-output_data_name = network_name + '_outputs.obj'
-network_name = network_name + '_test_path_cut' #'_learn1e-4_batch10'
-cp_folder_path = proj_folder + '/Neural_network/checkpoints/cp_' + \
-                        network_name + '_' + network_loss_function + '/' #+ '/next30'
-cp_folder_and_name = cp_folder_path + '/cp_{epoch:04d}'
 arch = load_architecture(network_name)
-loss = load_loss_function(network_loss_function, path_to_sys_file=path_to_system_description_file)
-nn_obj.architecture = arch
-nn_obj.loss_fn = loss
-nn_obj.l_rate = 1e-4
-set_params_and_init_nn(nn_obj, data_in_name=input_data_name, data_out_name=output_data_name, pickle_load=True)
-
-
-'''nn_regular_mse = NN()
-nn_custom_loss = NN()
-nn_square_loss = NN()
-nn_angle_loss = NN()
-list_of_nn_objs = [nn_regular_mse, nn_custom_loss, nn_square_loss, nn_angle_loss]'''
-
-
-# loss functions # NB: depend on output normalizer from NN, do not change this later on.
-'''custom_loss = CustomLoss(path=path_to_system_description_file, o_norm=nn_custom_loss.get_norm_output())
-custom_square_loss = SquaredLineFlowLoss(path=path_to_system_description_file, o_norm=nn_custom_loss.get_norm_output())
-only_angle_loss = LineFlowLossForAngle(path=path_to_system_description_file, o_norm=nn_custom_loss.get_norm_output())
-nn_regular_mse.loss_fn = tf.keras.losses.MSE
-nn_custom_loss.loss_fn = custom_loss
-nn_square_loss.loss_fn = custom_square_loss
-nn_angle_loss.loss_fn = only_angle_loss
-
-for model in list_of_nn_objs:
-    # NN parameters
-    model.epochs = 30
-    model.batch_size = 20
-    model.initializer = tf.keras.initializers.glorot_uniform(seed=0) #THIS IS THE SAME AS USED IN NON OBJ BASED APPROACH.
-    model.init_data('simple data.npy',
-                     'simple o data.npy',
-                     0.2,
-                     datapath=path_to_data,
-                     scale_data_out=True)
-    model.init_nn_model_dynamic(architecture=architecture, const_l_rate=True, custom_loss=False)'''
-
-'''text  = 'This is the testfile.'
-
-with open(cp_folder_path + 'textfile.txt', 'w') as f:
-    f.write(text)'''
-
-nn_obj.train_model(checkpoints=True,  cp_folder_path=cp_folder_and_name, save_freq=480*nn_obj.batch_size)
+remark = 'reference' # '_learn1e-4_batch10'
+nn_obj.l_rate = 1e-3
+batch_size = 20
+epochs = 150
 thresholds = [20, 10, 5, 3]
+network_loss_function = 'MSE' #CustomLoss, SquaredLineFlowLoss, LineFlowLossForAngle
+loss_function_list = ['MSE', 'CustomLoss']
+
+if dataset != '':
+    dataset = '_' + dataset + '_'
+
+input_data_name = network_name + dataset + '_inputs.obj'
+output_data_name = network_name + dataset +'_outputs.obj'
+
+nn_obj.architecture = arch
+folder_hierarchy['sys_descriptions'] = {}
+folder_hierarchy['checkpoints'] = {}
+
+folder_hierarchy['sys_descriptions']['container'] = path_to_system_description_file
+folder_hierarchy['sys_descriptions']['system_path'] = path_to_system_description_file + sys_filename
+folder_hierarchy['data_folder'] = path_data
+folder_hierarchy['checkpoints']['main_folder'] = path_to_cp_folder
+
+
+for loss_fun in loss_function_list:
+
+    cp_folder_path = path_to_cp_folder + '/cp_' + network_name + '_' + loss_fun + \
+                     '_' + remark + '/'  # + '/next30'
+    cp_folder_and_name = cp_folder_path + '/cp_{epoch:04d}'
+
+    loss = load_loss_function(loss_fun, path_to_sys_file=folder_hierarchy['sys_descriptions']['system_path'])
+    nn_obj.loss_fn = loss
+    
+    folder_hierarchy['checkpoints']['model_folder'] = path_to_cp_folder + '/' + network_name + '/' \
+                                                      + loss_fun + '_' + remark + '/'
+    folder_hierarchy['checkpoints']['model_storage_path'] = folder_hierarchy['checkpoints']['model_folder'] + \
+                                                            'cp_{epoch:04d}'
+
+    test = 2
+
+
+'''set_params_and_init_nn(nn_obj, data_in_name=input_data_name, data_out_name=output_data_name, pickle_load=True)
+
+
+nn_obj.train_model(checkpoints=True,
+                   epochs=epochs, batch_size=batch_size,
+                   cp_folder_path=cp_folder_and_name)#,
+                   #save_freq=480*nn_obj.batch_size)
+
 nn_obj.model_pred()
 nn_obj.generate_performance_data_dict_improved(thresholds)
 perf_dict_name = 'performance_epoch_' + str(nn_obj.epochs)
-store(nn_obj.performance_dict, path=cp_folder_path, filename=perf_dict_name)
+store(nn_obj.performance_dict, path=cp_folder_path, filename=perf_dict_name)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 nn_custom_loss.train_model(checkpoints=False,
