08d64dd11e9e330802e0f121abbd5ac9b5941cac
clmu <clmensmuller@tuta.io>
2023-04-17 12:16:43 +0200

    Improving user-friendliness

--- Neural_network/plot_performance.py
+++ Neural_network/plot_performance.py
@@ -4,40 +4,40 @@ from Neural_network.NN_objects import pickle_load_obj as load
 
 path = '/home/clemens/PycharmProjects/NN_LF_Topology/Neural_network/checkpoints/'
 network_size = 'medium'
-remark = 'baseline_slim'
+remark = 'slim_deep'
 
 loss_fun_list = ['MSE', 'CustomLoss']
-performance_data = []
+performance_data = {}
 
 for loss in loss_fun_list:
     datapath = path + network_size + '/' + loss + '_' + remark + '/'
-    performance_data.append(load(path=datapath, filename='perf_dict.obj'))
+    performance_data[loss] = load(path=datapath, filename='perf_dict_from_list.obj')
 
-epochs = len(performance_data[0])
-epoch_vec = np.arange(epochs)
+epochs = len(performance_data[loss_fun_list[0]])
+epoch_vec = np.arange(1, epochs+1)
 averages = [[], []]
 
 for epoch in range(epochs):
-    averages[0].append(performance_data[0][epoch]['overall_average'])
-    averages[1].append(performance_data[1][epoch]['overall_average'])
+    averages[0].append(performance_data[loss_fun_list[0]][epoch]['overall_average'])
+    averages[1].append(performance_data[loss_fun_list[1]][epoch]['overall_average'])
 
 def inches(cm):
     return cm/2.54
 
 fig_savepath = '/home/clemens/Dropbox/EMIL_MIENERG21/Master/Master/Figures/Results/'
 
-ylim = [0, 12]
+ylim = [0, 7]
 
-plt.plot(epoch_vec, averages[0], label=loss_fun_list[0])
-plt.plot(epoch_vec, averages[1], label=loss_fun_list[1])
+plt.plot(epoch_vec, averages[0], label=loss_fun_list[0], color='red')
+plt.plot(epoch_vec, averages[1], label=loss_fun_list[1], color='blue')
 plt.xlabel('Epoch number')
 plt.ylabel('Average percentage accuracy')
 plt.gca().set_ylim(ylim)
-plt.xticks(np.arange(min(epoch_vec), max(epoch_vec), 2))
+plt.xticks(np.arange(min(epoch_vec)-1, max(epoch_vec)-1, 5))
 plt.yticks(np.arange(min(ylim), max(ylim)+1, 0.5))
 plt.xticks()
 plt.legend()
 plt.grid()
 plt.gcf().set_size_inches(inches(16), inches(10))
-plt.savefig(fig_savepath + 'pinn_medium_baseline', dpi=600)
+plt.savefig(fig_savepath + 'pinn_' + network_size + '_' + remark, dpi=600)
 
