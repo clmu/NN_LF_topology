b425534a78e9692abc89b140cc648123b0330ff9
Clemens Müller <55401883+clmu@users.noreply.github.com>
2023-05-15 10:45:03 +0200

    Delete Tests directory

--- Tests/loadflow_test.py
+++
@@ -1,5 +0,0 @@
-import LF_3bus.ElkLoadFlow as elkLF
-import LF_3bus.build_sys_from_sheet as build
-
-BusList, LineList = build.BuildSystem('/home/clemens/PycharmProjects/NN_LF_Topology/LF_3bus/4 bus 1 gen.xls') #import from excel
-bus4 = elkLF.decoupled_LF(BusList, LineList) #create LF object
--- Tests/test_dataset_storage.py
+++
@@ -1,3 +0,0 @@
-from eiffel2 import builder as b
-
-b([6, 12, 12, 12, 6])
